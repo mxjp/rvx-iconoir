fbe68f34f6f337bb34d455b164cefe2c1abc892b
Max J. Polster <max.polster@id-p.de>
2026-04-26 16:47:55 +0200

    rvx 26

--- src/icon.tsx
+++ src/icon.tsx
@@ -1,12 +1,10 @@
-import { Component, SVG, uncapture, XMLNS } from "rvx";
+import { Component, leak, SVG, XMLNS } from "rvx";
 
 export function createIcon(variant: string, name: string, Template: Component<void, SVGSVGElement>): Component<void | {}, SVGSVGElement> {
 	let template: SVGSVGElement | undefined;
 	return () => {
 		if (template === undefined) {
-			template = XMLNS.inject(SVG, () => {
-				return uncapture(Template);
-			});
+			template = XMLNS.provide(SVG, () => leak(Template));
 			template.classList.add("iconoir", "iconoir-" + variant, "iconoir-" + name);
 		}
 		return template.cloneNode(true) as SVGSVGElement;
